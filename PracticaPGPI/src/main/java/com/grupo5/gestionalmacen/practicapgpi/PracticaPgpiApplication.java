67ced67a96846a3f0a41236672909153e392a89e
Alexcala7 <alexcalafat1234@gmail.com>
2024-04-12 18:38:58 +0200

    Commit Inicial

---
+++ PracticaPGPI/src/main/java/com/grupo5/gestionalmacen/practicapgpi/PracticaPgpiApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5.gestionalmacen.practicapgpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPgpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaPgpiApplication.class, args);
+    }
+
+}
---
+++ PracticaPGPI/src/test/java/com/grupo5/gestionalmacen/practicapgpi/PracticaPgpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo5.gestionalmacen.practicapgpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaPgpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
